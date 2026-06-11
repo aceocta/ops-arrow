205d6e2e8382f44a629be5e75c3b1c31e84ae938
suhirukpro <suhirukpro@gmail.com>
2026-06-11 11:30:42 +0100

    modifed

--- ScratchCard.Infrastructure/Persistence/ApplicationDbContextFactory.cs
+++ ScratchCard.Infrastructure/Persistence/ApplicationDbContextFactory.cs
@@ -9,8 +9,8 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applicati
     {
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
         var connectionString = Environment.GetEnvironmentVariable("DefaultConnection")
-        ?? "Server=localhost;Database=ops-arrow;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
-        //?? "Data Source=sql-opsarrow-uks.database.windows.net;Initial Catalog=sql-ops-arrow-uat;Persist Security Info=True;User ID=sysadmin;Password=Strange2025!;MultipleActiveResultSets=False;TrustServerCertificate=True;";
+        //?? "Server=localhost;Database=ops-arrow;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+        ?? "Data Source=sql-opsarrow-uks.database.windows.net;Initial Catalog=sql-ops-arrow-uat;Persist Security Info=True;User ID=sysadmin;Password=Strange2025!;MultipleActiveResultSets=False;TrustServerCertificate=True;";
 
         optionsBuilder.UseSqlServer(connectionString);
         return new ApplicationDbContext(optionsBuilder.Options);
